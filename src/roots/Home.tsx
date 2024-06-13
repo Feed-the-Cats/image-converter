0e0ae9da066dced8f4490c80f7397663a898186e
Feed-the-Cats <cyrildwd@gmail.com>
2024-06-13 20:25:47 +0200

    Fix bug

--- src/roots/Home.tsx
+++ src/roots/Home.tsx
@@ -8,6 +8,8 @@ const head = {
     "Convert Image to Base64, Convert Image to Blob, Convert Image to Data URL and Convert Image to File. You Can also convert images between different formats, apply artistic filters, crop images for a perfect result, and more.",
 };
 
-const Home: FC = (): JSX.Element => <HomeLayout isHome={"home"} />;
-useDocumentHead(head);
+const Home: FC = (): JSX.Element => {
+  useDocumentHead(head);
+  return <HomeLayout isHome={"home"} />;
+};
 export default Home;
